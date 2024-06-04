5e14388d449d6befc2eec3d0dd189aa66a16466e
Paulo Bolinhas <91274978+paulobolinhas@users.noreply.github.com>
2024-06-05 00:55:23 +0100

    Update DisinformationSimulatorGoogle.py

---
+++ DisinformationSimulatorGoogle.py
@@ -0,0 +1,2 @@
+# i want a Question Answering model LLM AI, with base, medium and large dataset, from google
+# that i could ask "what is euthanasia" on a python script and it give me an answer, for each level
